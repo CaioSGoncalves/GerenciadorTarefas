5a7aaa4d25a15f2ea86f4dcddaadb04cfebc981f
Caio Silva Gonçalves <caiosgon3@gmail.com>
2019-08-12 18:19:06 -0300

    Frontend Web finalizado

---
+++ web/src/pages/Main.js
@@ -0,0 +1,82 @@
+import React, { useEffect, useState } from 'react';
+import './Main.css';
+
+import api from '../services/api';
+
+export default function Main() {
+    const [ tasks, setTasks ] = useState([]);
+    const [ titulo, setTitulo ] = useState([]);
+
+    useEffect(() => {
+        async function loadTasks() {
+            const response = await api.get('/tarefas', {
+                headers: {
+                    todas: true,
+                }
+            });
+
+            setTasks(response.data);
+            console.log(response.data);
+        }
+        loadTasks();
+    }, [])
+
+    async function handleCadastroTask(e) {
+        e.preventDefault();
+
+        const response = await api.post('/tarefas', {
+            titulo,
+        });
+        alert("Tarefa criada com sucesso!")
+        console.log(response.data);
+    }
+
+    async function handleRowClick(e) {
+        e.preventDefault();
+        console.log("CLickou");
+    }
+
+    return (
+        <div className="main-container">
+            <div className="lista">
+                <h1>O que precisa ser feito?</h1>
+                { tasks.length > 0 ? (
+                    <ul>
+                    {tasks.map(task => (
+                        <li onClick={handleRowClick} key={task.id}>
+                            {/* <img src={task.avatar} alt="" /> */}
+                            <strong>{task.titulo}</strong>
+
+                            {/* <div className="buttons">
+                                <button type="button" onClick={() => handleDislike(user._id)}>
+                                    <img src={dislike} alt="Dislike" />
+                                </button>
+                                <button type="button" onClick={() => handleLike(user._id)}>
+                                    <img src={like} alt="Like" />
+                                </button>
+                            </div>      */}
+                        </li>
+                    ))}
+                    </ul>
+                ) : (
+                    <div className="empty">Acabou :)</div>
+                )}
+                
+            </div>
+
+            <div className="cadastro">
+                <h1>Cadastrar nova Tarefa</h1>
+                <form onSubmit={handleCadastroTask}>
+                    <input
+                    placeholder="Digite o titulo da Tarefa"
+                    value={titulo}
+                    onChange={e => setTitulo(e.target.value)}
+                    />
+                    <button type="submit">Cadastrar</button>
+                </form>
+
+        </div>
+        </div>
+    );
+}
+
